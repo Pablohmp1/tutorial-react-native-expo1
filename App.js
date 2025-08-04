6316517e0f0289a9a6e8a406ea5e0d5eb13f6ec3
Pablo Pires <pablo.pires@aluno.ifsp.edu.br>
2025-08-04 17:52:43 -0300

    Mechendo nos texts

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>HELLO WORLD!!!!!</Text>
+      <Text style={styles.tituloPrincipal}>HELLO WORLD!!!!!</Text>
+      <Text style={styles.subTitulo}>Subtitulo</Text>
+      <Text>Paragrafo</Text>
+      <Text>Saiba mais</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +20,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  tituloPrincipal: {
+    fontSize: '20px',
+    color: 'red',
+    fontWeight: 'bold',
+  },
+  subTitulo: {
+    fontSize: '10px',
+    color: 'green',
+    fontWeight: 'bold',
+    shadowColor: 'blue',
+    shadowRadius: '1, 5, 6',
+  }
+  
 });
